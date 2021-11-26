5ba7fe4570ce55f9e5493baf9dce528eb9baeee9
SJpricee <Shakthecoder@outlook.com>
2021-11-26 13:56:21 -0500

    Update US_Medical_Insurance_Costs.py

--- US_Medical_Insurance_Costs.py
+++ US_Medical_Insurance_Costs.py
@@ -5,5 +5,7 @@ with open('insurance.csv') as dataset :
     data = csv.DictReader(dataset)
     for i in data:
         names.append(i["age"])
-
+with open('insurance.csv') as insurance_csv_file:
+    pass
 print(names)
+
