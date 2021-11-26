1e58811b22b39da0bc7092cd96aa97387a5304eb
Ahmed Hanafy <ahmed_mahmoud4048@yahoo.com>
2021-11-26 21:14:22 +0200

    Insurance Class Added
    added an insurance class to work with in the future

--- US_Medical_Insurance_Costs.py
+++ US_Medical_Insurance_Costs.py
@@ -1,11 +1,22 @@
 import csv
 
+class Insurance :
+    def __init__(self, age, sex, bmi, children, smoker ):
+        self.age = age
+        self.sex = sex
+        self.bmi = bmi
+        self.children = children 
+        self.smoker = smoker
+
 with open('insurance.csv') as dataset :
     names = []
     data = csv.DictReader(dataset)
     for i in data:
         names.append(i["age"])
+
+
 with open('insurance.csv') as insurance_csv_file:
     pass
+
 print(names)
 
